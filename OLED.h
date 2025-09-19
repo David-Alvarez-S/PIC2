d492a70e96a3b444eb0ff2087fbc3deb77842eb7
David-Alvarez-S <david.alvarez.s@estudiantes.utec.edu.uy>
2025-09-19 02:06:09 -0300

    Se abstajo funciones de cumunicacion I2C y OLED

---
+++ OLED.h
@@ -0,0 +1,24 @@
+/*
+ * OLED.h
+ *
+ * Created: 19/9/2025 0:54:14
+ *  Author: David
+ */ 
+
+
+#ifndef OLED_H_
+#define OLED_H_
+
+void OLED_Command(uint8_t cmd);
+void OLED_Data(uint8_t data);
+void OLED_Init(void);
+void OLED_SetPosition(uint8_t page, uint8_t column);
+void OLED_Clear(void);
+void OLED_DrawChar(uint8_t page, uint8_t col, char c);
+void OLED_Print(uint8_t page, uint8_t col, const char *str);
+void Display(int Screen);
+
+
+
+
+#endif /* OLED_H_ */
